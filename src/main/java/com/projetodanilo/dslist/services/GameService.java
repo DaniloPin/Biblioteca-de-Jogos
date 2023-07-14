07e6084e8110ebab434a7876e3b124bd334412e2
Danilo Pinheiro <danilo.g.pinheiro@hotmail.com>
2023-07-14 11:35:12 -0300

    SQL queries and Projections

--- src/main/java/com/projetodanilo/dslist/services/GameService.java
+++ src/main/java/com/projetodanilo/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.projetodanilo.dslist.dto.GameDTO;
 import com.projetodanilo.dslist.dto.GameMinDTO;
 import com.projetodanilo.dslist.entities.Game;
+import com.projetodanilo.dslist.projections.GameMinProjection;
 import com.projetodanilo.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 	}
